fbbac41b0f5d6bf9f233b0056319862d515a9fb6
Shun Takamura <shun@ShunnoMacBook-Air.local>
2026-03-25 04:08:41 -0700

    WIP

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -2,8 +2,22 @@ import Item from './Item';
 import NoteList from '../NoteList';
 import UserItem from './UserItem';
 import { FiPlus, FiSearch } from 'react-icons/fi';
+import { noteRepository } from '../../modules/notes/note.repository';
 
 export default function SideBar() {
+  /**
+   * ノート作成
+   * @returns { Promise<void> } ノートの作成完了を表すPromiseオブジェクト
+   */
+  const createNote = async (): Promise<void> => {
+    try {
+      await noteRepository.create({ title: '無題' });
+    } catch (error) {
+      console.error(error);
+      alert('ノートの作成に失敗しました');
+    }
+  }
+
   return (
     <>
       <aside className='sidebar'>
@@ -18,7 +32,7 @@ export default function SideBar() {
           </div>
           <div className='sidebar-spacer'>
             <NoteList />
-            <Item label='ノートを作成' icon={FiPlus} onClick={() => {}} />
+            <Item label='ノートを作成' icon={FiPlus} onClick={createNote} />
           </div>
         </div>
       </aside>
